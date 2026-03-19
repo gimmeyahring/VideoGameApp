7ab235ab0974159d810a774b0722294ee2dabee1
1155381 <donavanriddick08@gmail.com>
2026-03-19 15:35:17 -0400

    Initalized all xml stuff in main activity

--- app/src/main/java/org/baltimorecityschools/videogameapp/GameActivity.java
+++ app/src/main/java/org/baltimorecityschools/videogameapp/GameActivity.java
@@ -1,19 +1,42 @@
 package org.baltimorecityschools.videogameapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GameActivity extends AppCompatActivity {
 
+    Game peak;
+
+    ConstraintLayout mainLayout;
+    ScrollView gameScrollView;
+    ConstraintLayout scrollViewLayout;
+    ImageView heroBannerIV;
+    ImageView heroThumbnailIV;
+    TextView gameTitleTV;
+    TextView gameDescriptionTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        mainLayout = findViewById(R.id.main);
+        gameScrollView = findViewById(R.id.gamescrollviewid);
+        scrollViewLayout = findViewById(R.id.mainLayout2);
+        heroBannerIV = findViewById(R.id.herobannerid);
+        heroThumbnailIV = findViewById(R.id.herothumbnailid);
+        gameTitleTV = findViewById(R.id.gametitleTextview);
+        gameDescriptionTV = findViewById(R.id.gameexpandabledescriptionTextview);
+
+
 
 
     }
