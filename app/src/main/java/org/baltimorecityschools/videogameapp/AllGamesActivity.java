33efc7a48202eb44e49ecf4bd14f75edfe0d563f
1155381 <donavanriddick08@gmail.com>
2026-03-23 15:52:52 -0400

    Started my All games XML and fixed my android studio

---
+++ app/src/main/java/org/baltimorecityschools/videogameapp/AllGamesActivity.java
@@ -0,0 +1,19 @@
+package org.baltimorecityschools.videogameapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AllGamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_games);
+
+    }
+}
